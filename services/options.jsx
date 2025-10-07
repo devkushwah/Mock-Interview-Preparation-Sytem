3ca1b31fd53af6fed51497bfe70037c871a20ad7
Dev Kushwah <devkushwah880@gmail.com>
2025-10-07 11:48:43 +0530

    feat: add interview options service and context management

---
+++ services/options.jsx
@@ -0,0 +1,42 @@
+export const ExpertsList = [
+    {
+        name: "Mock Interview",
+        description: "Get personalized feedback on your interview skills from industry experts.",
+        icon: "/icons/interviewImage.jpg"
+    },
+
+    {
+        name: "TopicWise Preparation",
+        description: "Engage in thoughtful discussions to enhance your communication and critical thinking skills.",
+        icon: "/icons/topicwiseprep.jpg"
+    },
+
+    {
+        name: "Ques- Answer Practice",
+        description: "Practice answering common interview questions to build confidence and improve your responses.",
+        icon: "/icons/quesAndansprep.png"
+    },
+
+    {
+        name: "English Practice",
+        description: "Improve your English speaking skills through interactive conversations and exercises.",
+        icon: "/icons/EnglishPractice.png"
+    }
+]
+
+export const Interviewer = [
+    {
+        name: 'John Doe',
+        avatar: '/avatars/avatar1.jpg'
+    },
+
+     {
+        name: 'John Doe',
+        avatar: '/avatars/avatar2.jpg'
+    },
+
+     {
+        name: 'John Doe',
+        avatar: '/avatars/avatar3.jpg'
+    }
+]
