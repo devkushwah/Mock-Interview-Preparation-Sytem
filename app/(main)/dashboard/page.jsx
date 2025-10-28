546b6354e1cc502ca20d18cf6c8b65218ffefd66
Dev Kushwah <devkushwah880@gmail.com>
2025-10-28 11:13:48 +0530

    FixUpdation of Credits

--- app/(main)/dashboard/page.jsx
+++ app/(main)/dashboard/page.jsx
@@ -1,20 +1,23 @@
-import React from 'react'
+'use client'
+
+import { useContext } from 'react'
+import { UserContext } from '@/app/_context/UserContext'
 import History from './_components/History'
 import PracticePartner from './_components/PracticePartner'
 
 
-const Dashboard = () => {
+export default function Dashboard() {
+  const { userData } = useContext(UserContext)
+
   return (
     <div>
-
+      <p className='text-gray-600 mt-1'>Total Credits: {userData?.credit || 0}</p>
       <PracticePartner />
 
       <div className='grid grid-cols-1 md:grid-cols-2 gap-10 mt-10'>
         <History />
-       
+        
       </div>
     </div>
   )
 }
-
-export default Dashboard
