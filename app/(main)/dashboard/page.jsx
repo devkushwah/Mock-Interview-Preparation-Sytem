db330d557954b54fa17dca7fb0f5c02984750dd1
Dev Kushwah <devkushwah880@gmail.com>
2025-11-02 16:54:32 +0530

    Fiexed Spacing and UI

--- app/(main)/dashboard/page.jsx
+++ app/(main)/dashboard/page.jsx
@@ -10,14 +10,12 @@ export default function Dashboard() {
   const { userData } = useContext(UserContext)
 
   return (
-    <div>
-      <p className='text-gray-600 mt-1'>Total Credits: {userData?.credit || 0}</p>
-      <PracticePartner />
+    <div className='space-y-12'>
+      <PracticePartner credits={userData?.credit || 0} />
 
-      <div className='grid grid-cols-1 md:grid-cols-2 gap-10 mt-10'>
+      <section className='grid grid-cols-1 gap-10'>
         <History />
-        
-      </div>
+      </section>
     </div>
   )
 }
